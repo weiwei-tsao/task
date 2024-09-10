472c82e0d6192f354ab0bd4b18ae3cdd2ae6e767
weiwei-tsao <weiwei.tsao@hotmail.com>
2024-09-10 11:13:58 -0400

    feat: add utils for setting up logs

--- src/utils.py
+++ src/utils.py
@@ -7,5 +7,9 @@ def setup_logging(log_dir="./logs"):
     if not os.path.exists(log_dir):
         os.mkdir(log_dir)
 
-    log_filename = os.path.join(log_dir, f"sftp_xml_to_json_s3_{datetime.now().strftime("%Y%m%d%-H%M%S")}.log")
-    logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s %(message)s')
+    log_filename = os.path.join(
+        log_dir, f"sftp_xml_to_json_s3_{datetime.now().strftime('%Y%m%d%-H%M%S')}.log"
+    )
+    logging.basicConfig(
+        filename=log_filename, level=logging.INFO, format="%(asctime)s %(message)s"
+    )
